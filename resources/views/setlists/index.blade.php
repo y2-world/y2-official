1e4016e3d72ef5a35c2ba4df59fffafe0e870f49
Yuki Yoshida <yuki92496@gmail.com>
2022-06-13 22:20:34 +0900

    all setlists width revised

--- resources/views/setlists/index.blade.php
+++ resources/views/setlists/index.blade.php
@@ -28,20 +28,22 @@
           <th>開催日</th>
           <th>アーティスト名</th>
           <th>ツアータイトル</th>
-          <th>会場</th>
+          <th class="pc_list">会場</th>
         </tr>
       </thead>
-      <tbody>
-          @foreach ($setlists as $setlist)
-          <tr>
-              <td></td>
-              <td>{{ date('Y.m.d', strtotime($setlist->date)) }}</td>
-              <td><a href="{{ url('artists', $setlist->artist_id)}}">{{ $setlist->artist->name }}</a></td>
-              <td><a href="{{ route('setlists.show', $setlist->id) }}">{{ $setlist->tour_title }}</a></td>
-              <td>{{ $setlist->venue }}</td>
-          </tr>
-          @endforeach
-      </tbody>
+      <div class="all-setlist">
+        <tbody>
+            @foreach ($setlists as $setlist)
+            <tr>
+                <td></td>
+                <td>{{ date('Y.m.d', strtotime($setlist->date)) }}</td>
+                <td><a href="{{ url('artists', $setlist->artist_id)}}">{{ $setlist->artist->name }}</a></td>
+                <td><a href="{{ route('setlists.show', $setlist->id) }}">{{ $setlist->tour_title }}</a></td>
+                <td class="pc_list">{{ $setlist->venue }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+      </div>
   </table>
 </div>
 @endsection
