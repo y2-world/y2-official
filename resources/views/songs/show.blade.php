021441286c4a3cbcea29dd69aecd91193f543d4a
Yuki Yoshida <yuki92496@gmail.com>
2022-06-28 20:11:47 +0900

    correction

--- resources/views/songs/show.blade.php
+++ resources/views/songs/show.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            # {{$songs->id}}
             <h3> {{$songs->title}}</h3>
             <hr>
             <br>
