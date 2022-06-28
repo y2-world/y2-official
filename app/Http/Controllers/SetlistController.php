dd4c26e2a016714dde05822be23653ef8ba87d42
Yuki Yoshida <yuki92496@gmail.com>
2022-06-28 21:45:49 +0900

    correction

--- app/Http/Controllers/SetlistController.php
+++ app/Http/Controllers/SetlistController.php
@@ -16,7 +16,7 @@ class SetlistController extends Controller
     public function index()
     {   
         $setlists = Setlist::orderBy('date', 'desc')
-        ->paginate(10);
+        ->paginate(25);
         $artists = Artist::orderBy('created_at', 'asc')
         ->get();
         return view('setlists.index', compact('artists', 'setlists'));
