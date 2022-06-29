c0260f8844b8007fe5b96cf9c2c74355877f4859
Yuki Yoshida <yuki92496@gmail.com>
2022-06-29 23:00:00 +0900

    correction

--- resources/views/songs/show.blade.php
+++ resources/views/songs/show.blade.php
@@ -13,7 +13,9 @@
             Album : <a href="{{ route('albums.show', $songs->album_id) }}">{{ $songs->album->title }}</a></td>
             @endif
             <br>
+            @if(isset($albums))
             Release Date : {{ date('Y.m.d', strtotime($songs->album->date)) }}
+            @endif
             <br>
             <br>
             <div class="show_button">
