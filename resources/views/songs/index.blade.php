d39df08a221f06252e92596788f8d2d6bc07ca5f
Yuki Yoshida <yuki92496@gmail.com>
2022-06-29 21:29:11 +0900

    correction

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -12,18 +12,20 @@
         </tr>
       </thead>
       <div class="all-setlist">
-        <tbody>
+        
             @foreach ($songs as $song)
               @if(!is_null($song->song_id))
+              <tbody>
                 <tr>
                     <td>{{$song->song_id}}</td>
                     <td><a href="{{ route('songs.show', $song->id) }}">{{ $song->title }}</a></td>
                     {{-- <td>{{ $song->album->title }}</td> --}}
                     {{-- <td><a href="{{ route('albums.show', $song->album_id) }}">{{ $song->album->title }}</a></td> --}}
                 </tr>
+              </tbody>
               @endif
             @endforeach
-        </tbody>
+       
       </div>
     </table>
   <div class=”pagination”>
