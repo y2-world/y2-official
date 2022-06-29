8d2e9d05bdba68e4b7a7494eef06bcad56c48fb7
Yuki Yoshida <yuki92496@gmail.com>
2022-06-29 22:41:52 +0900

    correction

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -8,7 +8,7 @@
         <tr>
           <th class="mb_list">#</th>
           <th class="mb_list">タイトル</th>
-          {{-- <th class="mb_list">アルバム</th> --}}
+          <th class="mb_list">アルバム</th>
         </tr>
       </thead>
       <div class="all-setlist">
@@ -17,8 +17,7 @@
                 <tr>
                   <td>{{$song->song_id}}</td>
                   <td><a href="{{ route('songs.show', $song->id) }}">{{ $song->title }}</a></td>
-                  {{-- <td>{{ $song->album->title }}</td> --}}
-                  {{-- <td><a href="{{ route('albums.show', $song->album_id) }}">{{ $song->album->title }}</a></td> --}}
+                  <td><a href="{{ route('albums.show', $song->album_id) }}">{{ $song->album->title }}</a></td>
                 </tr>
             @endforeach
         </tbody>
