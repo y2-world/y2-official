53332a48f2eb2c56f168310320331467665bfd0a
Yuki Yoshida <yuki92496@gmail.com>
2022-07-11 22:43:27 +0900

    correction

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -22,8 +22,12 @@
                   <td>{{$song->song_id}}</td>
                   <td><a href="{{ route('songs.show', $song->id) }}">{{ $song->title }}</a></td>
                   @if(isset($song->single_id) && isset($song->album_id))
-                  <td><a href="{{ route('singles.show', $song->single_id) }}">{{ $song->single->title }}</a></td>
-                  <td class="pc_list">{{ date('Y.m.d', strtotime($song->single->date)) }}</td>
+                    @if($song->single->date > $song->album->date)
+                    <td><a href="{{ route('albums.show', $song->album_id) }}">{{ $song->album->title }}</a></td>
+                    <td class="pc_list">{{ date('Y.m.d', strtotime($song->album->date)) }}</td>
+                    @else
+                    <td><a href="{{ route('singles.show', $song->single_id) }}">{{ $song->single->title }}</a></td>
+                    <td class="pc_list">{{ date('Y.m.d', strtotime($song->single->date)) }}</td>
                   @elseif(isset($song->album_id))
                     <td><a href="{{ route('albums.show', $song->album_id) }}">{{ $song->album->title }}</a></td>
                     <td class="pc_list">{{ date('Y.m.d', strtotime($song->album->date)) }}</td>
