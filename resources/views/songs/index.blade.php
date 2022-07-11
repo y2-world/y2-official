2b3dbcb8c2fa73a4809f02a9b341070d869c6dc8
Yuki Yoshida <yuki92496@gmail.com>
2022-07-11 22:22:45 +0900

    correction

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -22,7 +22,7 @@
                   <td>{{$song->song_id}}</td>
                   <td><a href="{{ route('songs.show', $song->id) }}">{{ $song->title }}</a></td>
                   @if(isset($song->single_id))
-                  <td><a href="{{ route('albums.show', $song->single_id) }}">{{ $song->single->title }}</a></td>
+                  <td><a href="{{ route('singles.show', $song->single_id) }}">{{ $song->single->title }}</a></td>
                   <td class="pc_list">{{ date('Y.m.d', strtotime($song->single->date)) }}</td>
                   @elseif(isset($song->album_id))
                     <td><a href="{{ route('albums.show', $song->album_id) }}">{{ $song->album->title }}</a></td>
