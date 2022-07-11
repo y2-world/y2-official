e2ebad1c43f55ba542637a7f76f68a1faddbd5f9
Yuki Yoshida <yuki92496@gmail.com>
2022-07-11 21:58:13 +0900

    correction

--- resources/views/singles/index.blade.php
+++ resources/views/singles/index.blade.php
@@ -20,7 +20,7 @@
             @foreach ($singles as $single)
               <tr>
                   <td>{{$single->single_id}}</td>
-                  <td><a href="{{ route('albums.show', $single->id) }}">{{ $single->title }}</a></td>
+                  <td><a href="{{ route('singles.show', $single->id) }}">{{ $single->title }}</a></td>
                   <td>{{ date('Y.m.d', strtotime($single->date)) }}</td>
               </tr>
             @endforeach
