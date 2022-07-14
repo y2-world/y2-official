f3b0eb789b504645ce0732a984fe4c29f59b13eb
Yuki Yoshida <yuki92496@gmail.com>
2022-07-14 23:57:56 +0900

    correction

--- resources/views/bios/index.blade.php
+++ resources/views/bios/index.blade.php
@@ -16,19 +16,6 @@
     </div>
   </div>
   <hr>
-  <h4 class="bio">Biography</h4>
-  <div class="row justify-content-center">
-    <div class="col-md-6">
-      <div class="year-wrapper">
-          @foreach ($bios as $bio)
-          <div class="year-padding">
-          <a class="btn btn-outline-dark btn-sm" href="{{ url('bios', $bio->id)}}" role="button">{{ $bio->year }}</a>
-          </div>
-          @endforeach
-      </div>
-    </div>
-  </div>
-  <br>
   <h4 class="bio">Discography</h4>
   <div class="row justify-content-center">
     <div class="col-md-6">
@@ -38,6 +25,32 @@
             <a class="btn btn-outline-dark btn-sm" href="{{ url('/singles') }}" role="button">Singles</a>
             <a class="btn btn-outline-dark btn-sm" href="{{ url('/albums') }}" role="button">Albums</a>
           </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <h4 class="bio">Live</h4>
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <div class="year-wrapper">
+          <div class="year-padding">
+            <a class="btn btn-outline-dark btn-sm" href="{{ url('/tours') }}" role="button">Tours</a>
+            <a class="btn btn-outline-dark btn-sm" href="{{ url('/events') }}" role="button">Events</a>
+            <a class="btn btn-outline-dark btn-sm" href="{{ url('/aps') }}" role="button">ap bank fes</a>
+          </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <h4 class="bio">Biography</h4>
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <div class="year-wrapper">
+          @foreach ($bios as $bio)
+          <div class="year-padding">
+          <a class="btn btn-outline-dark btn-sm" href="{{ url('bios', $bio->id)}}" role="button">{{ $bio->year }}</a>
+          </div>
+          @endforeach
       </div>
     </div>
   </div>
