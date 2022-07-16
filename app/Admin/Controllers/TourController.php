ef9b98fbc4b63b034b2a1f043b36b7cd43c98ac0
Yuki Yoshida <yuki92496@gmail.com>
2022-07-17 00:33:26 +0900

    tour added correction

--- app/Admin/Controllers/TourController.php
+++ app/Admin/Controllers/TourController.php
@@ -86,12 +86,12 @@ class TourController extends AdminController
         $form->text('year', __('年'));
         $form->table('setlist', __('本編'), function ($table) {
             $table->number('#')->rules('required');
-            $table->select('song', __('楽曲'))->options(Song::all()->pluck('title', 'id'))->rules('required');
+            $table->select('song', __('楽曲'))->options(Song::all()->pluck('title', 'id'));
             $table->text('exception', __('例外'));
         });
         $form->table('encore', __('アンコール'), function ($table) {
             $table->number('#')->rules('required');
-            $table->select('song', __('楽曲'))->options(Song::all()->pluck('title', 'id'))->rules('required');
+            $table->select('song', __('楽曲'))->options(Song::all()->pluck('title', 'id'));
             $table->text('exception', __('例外'));
         });
         $form->textarea('text', __('コメント'));
