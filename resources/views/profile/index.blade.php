846d551de0e13ff191c9cf28e7f18d35056ad519
Yuki Yoshida <yuki92496@gmail.com>
2022-08-01 00:15:37 +0900

    news added

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -7,22 +7,22 @@
             <h2>Profile</h2>
                 <div class="element js-fadein"> 
                     <div class="row">
-                        <div class="col-md-6 my-auto">
-                            <div class="prof_img">
-                                <img src={{ asset('images/'. $profiles->image) }} class="image" width="80%">
-                            </div>
-                        </div>
-                        <div class="col-md-5 my-auto"> 
-                            @if($profiles)
-                                @foreach ($profiles as $profile)
+                        @if($profiles)
+                            @foreach ($profiles as $profile)
+                                <div class="col-md-6 my-auto">
+                                    <div class="prof_img">
+                                        <img src={{ asset('images/'. $profile->image) }} class="image" width="80%">
+                                    </div>
+                                </div>
+                                <div class="col-md-5 my-auto"> 
                                     <br>
                                     <h2>{{$profile->name}}</h2>
                                     <p>{{$profile->info}}<p>
                                     <hr>
                                     <p class="profile-text">{!! nl2br(e($profile->text)) !!}</p>
                                     <hr>
-                                @endforeach
-                            @endif
+                            @endforeach
+                        @endif
                         </div>
                     </div>
                 </div>
