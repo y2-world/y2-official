5da692831898186b679f0ad2e1e5af97d4c28617
Yuki <yuki92496@gmail.com>
2023-05-08 22:33:21 +0900

    index revised

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -14,14 +14,14 @@
                 <h2 class="news-title">New Release</h2>
                 <div class="cover-wrapper">
                     @foreach ($discos as $disco)
-                    <?php date('Y-m-d', strtotime($disco->date)) <= date('Y-m-d'); ?>
+                    @if (strtotime($disco->date) < strtotime(date('Y-m-d')))
                     <div class="disc-block">
                         <img src={{ asset('https://res.cloudinary.com/hqrgbxuiv/'. $disco->image) }} class="top-image">
                         <br><br>
                         <div class="topic"><a href="{{ route('music.show', $disco->id) }}">{{$disco->title}}</a></div>
                         <p class="topic">{{ date('Y.m.d', strtotime($disco->date)) }} - {{$disco->subtitle}}</p>
                     </div>
-                    <?php endif;?>
+                    @endif
                     @endforeach
                 </div>
             </div>
