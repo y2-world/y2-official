735157cf91e5573e2cee1a2c9dbe93a6d8eae35b
Yuki <yuki92496@gmail.com>
2023-05-14 22:05:24 +0900

    nav-bar link adjusted

--- public/js/main.js
+++ public/js/main.js
@@ -52,3 +52,11 @@
         });
     });
 }
+{
+    $(window).on("load resize", function () {
+        // navbarの高さを取得する
+        var height = $(".navbar").height();
+        // bodyのpaddingにnavbarの高さを設定する
+        $("body").css("padding-top", height);
+    });
+}
