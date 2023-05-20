b2cce65b5ab72a7caa685de0d4ab2c04468b9e1c
Yuki <yuki92496@gmail.com>
2023-05-20 19:00:15 +0900

    home css revised

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -117,7 +117,7 @@ $today = strtotime(date('Y-m-d'));
             <div class="col-md-11">
                 <div class="element js-fadein">
                     <h2>Yuki Yoshida Official Fanclub『NAPO』</h2>
-                    <h3>NAPO = Neo Artist Personal Official Fanclub</h5>
+                    <h5>NAPO = Neo Artist Personal Official Fanclub</h5>
                 </div>
                 <br>
                 <div class="element js-fadein">
