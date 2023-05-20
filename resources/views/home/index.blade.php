effc6f4e133789ffccab78069de99fc8f0d3150c
Yuki <yuki92496@gmail.com>
2023-05-20 18:55:32 +0900

    home css revised

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -116,7 +116,7 @@ $today = strtotime(date('Y-m-d'));
         <div class="row justify-content-center">
             <div class="col-md-11">
                 <div class="element js-fadein">
-                    <h2>Yuki Yoshida Official Fanclub『NAPO』</h2>
+                    <h2 style="text-align:center;">Yuki Yoshida Official Fanclub『NAPO』</h2>
                     <h3 style="text-align:center;">NAPO = Neo Artist Personal Official Fanclub</h5>
                 </div>
                 <br>
@@ -171,19 +171,4 @@ $today = strtotime(date('Y-m-d'));
         </div>
     </div>
 </div>
-
-<!-- <script>
-{
-    $(function () {
-        $('a[href^="#"]').not('.remove-class').click(function () {
-            var speed = 500;
-            var href = $(this).attr("href");
-            var target = $(href == "#" || href == "" ? "html" : href);
-            var position = target.offset().top;
-            $("html, body").animate({ scrollTop: position }, speed, "swing");
-            return false;
-        });
-    });
-}
-</script> -->
 @endsection
