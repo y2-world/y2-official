6542802f46e554d55737acb923f82dd802bb2e14
Yuki <yuki92496@gmail.com>
2023-05-30 23:35:48 +0900

    React 表示テスト

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,3 +13,14 @@ import './bootstrap';
  */
 
 import './components/Example';
+
+require('./bootstrap');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Example from './components/Example';
+
+
+if (document.getElementById('app')) {
+  ReactDOM.render(<Example />, document.getElementById('app'));
+}
