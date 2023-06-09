f6b441633da7869b8df9d07cf2abb7e57997b28d
Yuki <yuki92496@gmail.com>
2023-06-09 21:08:59 +0900

    メニュー変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@
                 </li>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/setlists') }}">&emsp;LIVE×YOU</a>
+                    <a class="nav-link" href="{{ url('/setlists') }}">&emsp;Set List</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('/bios') }}">&emsp;Database</a>
