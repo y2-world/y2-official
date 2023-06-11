b23b77b1a0b724f91dfbf239b3ad825da1784d10
Yuki <yuki92496@gmail.com>
2023-06-11 20:26:27 +0900

    タイトル変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Yuki Yoshida Official Website') }}</title>
 
     <!-- Fonts -->
     <script src="https://kit.fontawesome.com/e47a10189c.js" crossorigin="anonymous"></script>
