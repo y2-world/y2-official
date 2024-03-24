9ae4cc98dcf3ab683be3b8275af6af0bcc4812e9
Yuki <yuki92496@gmail.com>
2024-03-24 19:12:33 +0900

    visible erased

--- resources/views/setlists/index.blade.php
+++ resources/views/setlists/index.blade.php
@@ -9,7 +9,9 @@
         <option value="" disabled selected>Artists</option>
         <option value="{{ url('/artists')}}">All Artists</option>
         @foreach ($artists as $artist)
+        @if($artist->visible != 1)
         <option value="{{ url('/artists', $artist->id)}}">{{ $artist->name }}</option>
+        @endif
         @endforeach
       </select>
       <select name="select" onChange="location.href=value;">
@@ -27,7 +29,7 @@
             <select name="artist_id" required data-toggle="select"> 
               <option value="" disabled selected>Artists</option>
                 @foreach ($artists as $artist)
-                @if($artist->visible !== 1)
+                @if($artist->visible != 1)
                     <option value="{{ $artist->id }}" required>{{$artist->name}}</option>
                 @endif
                 @endforeach
