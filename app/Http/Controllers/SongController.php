edfdd9f47d8ff7a9c1c0d959dcf1f661c738fcc5
Yuki <yuki92496@gmail.com>
2024-04-12 00:25:04 +0900

    live history added

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -62,6 +62,7 @@ class SongController extends Controller
         $singles = Single::orderBy('id', 'asc')
         ->get();
         $tours = Tour::whereRaw("JSON_EXTRACT(setlist1, '$.*.id') REGEXP '\"$id\"'")
+        ->orWhereRaw("JSON_EXTRACT(setlist2, '$.*.id') REGEXP '\"$id\"'")
         ->orderBy('date1', 'desc')
         ->get();
 
