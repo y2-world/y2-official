95a0ea36646fceb5536d2d8764c070f18cc15192
Yuki <yuki92496@gmail.com>
2024-04-16 20:55:22 +0900

    new search added

--- app/Http/Controllers/SetlistController.php
+++ app/Http/Controllers/SetlistController.php
@@ -16,14 +16,38 @@ class SetlistController extends Controller
      */
     public function index()
     {   
-        $setlists = Setlist::orderBy('date', 'desc')
-        ->paginate(10);
-        $artists = Artist::where('visible', 0)->orderBy('id', 'asc')
-        ->get();
-        $allArtists = Artist::orderBy('id', 'asc')
-        ->get();
-        $years = Year::orderBy('year', 'asc')
-        ->get();
+        // $setlists = Setlist::orderBy('date', 'desc')
+        // ->paginate(10);
+        // $artists = Artist::where('visible', 0)->orderBy('id', 'asc')
+        // ->get();
+        // $allArtists = Artist::orderBy('id', 'asc')
+        // ->get();
+        // $years = Year::orderBy('year', 'asc')
+        // ->get();
+        // return view('setlists.index', compact('artists', 'allArtists', 'setlists', 'years'));
+
+        $liveType = request()->input('live_type');
+
+        // クエリビルダーを生成し、セットリストを取得する
+        $setlistsQuery = Setlist::orderBy('date', 'desc');
+    
+        if ($liveType === '1') {
+            // live_typeが1の場合はfesカラムが0のセットリストを取得する
+            $setlistsQuery->where('fes', 0);
+        } elseif ($liveType === '2') {
+            // live_typeが2の場合はfesカラムが1か2のセットリストを取得する
+            $setlistsQuery->whereIn('fes', [1, 2]);
+        }
+    
+        // ページネーションを適用してセットリストを取得する
+        $setlists = $setlistsQuery->paginate(10);
+    
+        // アーティスト、全てのアーティスト、年のデータを取得する
+        $artists = Artist::where('visible', 0)->orderBy('id', 'asc')->get();
+        $allArtists = Artist::orderBy('id', 'asc')->get();
+        $years = Year::orderBy('year', 'asc')->get();
+    
+        // ビューにデータを渡して表示する
         return view('setlists.index', compact('artists', 'allArtists', 'setlists', 'years'));
     }
 
--- resources/views/setlists/index.blade.php
+++ resources/views/setlists/index.blade.php
@@ -1,85 +1,95 @@
 @extends('layouts.app')
 @section('content')
-<br>
-<div class="container-lg">
-  <h2>Set Lists</h2>
-  <div class="parts-wrapper">
-    <div class="dropdown-wrapper">
-      <select name="select" onChange="location.href=value;">
-        <option value="" disabled selected>Artists</option>
-        <option value="{{ url('/artists')}}">All Artists</option>
-        @foreach ($artists as $artist)
-        <option value="{{ url('/artists', $artist->id)}}">{{ $artist->name }}</option>
-        @endforeach
-      </select>
-      <select name="select" onChange="location.href=value;">
-        <option value="" disabled selected>Years</option>
-        <option value="{{ url('/years')}}">All Years</option>
-        @foreach ($years as $year)
-        <option value="{{ url('/years', $year->id)}}">{{ $year->year }}</option>
-        @endforeach
-      </select>
-      </select>
+    <br>
+    <div class="container-lg">
+        <h2>Set Lists</h2>
+        <div class="parts-wrapper">
+            <div class="dropdown-wrapper">
+                <select name="select" onChange="location.href=value;">
+                    <option value="" disabled selected>Artists</option>
+                    <option value="{{ url('/artists') }}">All Artists</option>
+                    @foreach ($artists as $artist)
+                        <option value="{{ url('/artists', $artist->id) }}">{{ $artist->name }}</option>
+                    @endforeach
+                </select>
+                <select name="select" onChange="location.href=value;">
+                    <option value="" disabled selected>Years</option>
+                    <option value="{{ url('/years') }}">All Years</option>
+                    @foreach ($years as $year)
+                        <option value="{{ url('/years', $year->id) }}">{{ $year->year }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="pc_list">
+                <div class="search">
+                    <form action="{{ url('/search') }}" method="GET">
+                        <div class="mb_dropdown">
+                            <select name="artist_id" required data-toggle="select">
+                                <option value="" disabled selected>Artists</option>
+                                @foreach ($allArtists as $artist)
+                                    <option value="{{ $artist->id }}" required>{{ $artist->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="search" class="form-control" aria-label="Search" value="{{ request('keyword') }}"
+                                name="keyword" required>
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <ul class="music-menu">
+            <li><a href="{{ url('/setlists') }}" class="{{ request('live_type') ? '' : 'active' }}">All</a></li>
+            <li><a href="{{ url('/setlists?live_type=1') }}"
+                    class="{{ request('live_type') == '1' ? 'active' : '' }}">Live</a></li>
+            <li><a href="{{ url('/setlists?live_type=2') }}"
+                    class="{{ request('live_type') == '2' ? 'active' : '' }}">Fes</a></li>
+        </ul>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th class="mb_list">#</th>
+                    <th class="mb_list">開催日</th>
+                    <th class="sp">アーティスト / タイトル</th>
+                    <th class="pc_list">アーティスト</th>
+                    <th class="pc_list">タイトル</th>
+                    <th class="pc_list">会場</th>
+                </tr>
+            </thead>
+            <div class="all-setlist">
+                <tbody>
+                    @foreach ($setlists as $setlist)
+                        <tr>
+                            <td>{{ $setlist->id }}</td>
+                            <td>{{ date('Y.m.d', strtotime($setlist->date)) }}</td>
+                            @if (isset($setlist->artist_id))
+                                <td class="pc_list">
+                                    <a href="{{ url('artists', $setlist->artist_id) }}">{{ $setlist->artist->name }}</a>
+                                </td>
+                                <td class="sp">
+                                    <a href="{{ url('artists', $setlist->artist_id) }}">{{ $setlist->artist->name }}</a> /
+                                    <a href="{{ route('setlists.show', $setlist->id) }}">{{ $setlist->title }}</a>
+                                </td>
+                            @else
+                                <td class="pc_list"></td>
+                                <td class="sp"><a
+                                        href="{{ route('setlists.show', $setlist->id) }}">{{ $setlist->title }}</a></td>
+                            @endif
+                            <td class="pc_list"><a
+                                    href="{{ route('setlists.show', $setlist->id) }}">{{ $setlist->title }}</a></td>
+                            <td class="pc_list">{{ $setlist->venue }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </div>
+        </table>
+        <div class=”pagination”>
+            {!! $setlists->links() !!}
+        </div>
+        <br>
     </div>
-    <div class="pc_list">
-      <div class="search">
-        <form action="{{url('/search')}}" method="GET">
-          <div class="mb_dropdown">
-            <select name="artist_id" required data-toggle="select"> 
-              <option value="" disabled selected>Artists</option>
-                @foreach ($allArtists as $artist)
-                    <option value="{{ $artist->id }}" required>{{$artist->name}}</option>
-                @endforeach
-            </select>
-          </div>
-          <div class="input-group mb-3">
-              <input type="search" class="form-control" aria-label="Search" value="{{request('keyword')}}" name="keyword" required>
-              <div class="input-group-append">
-                  <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
-              </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-  <table class="table table-striped">
-      <thead>
-        <tr>
-          <th class="mb_list">#</th>
-          <th class="mb_list">開催日</th>
-          <th class="sp">アーティスト / タイトル</th>
-          <th class="pc_list">アーティスト</th>
-          <th class="pc_list">タイトル</th>
-          <th class="pc_list">会場</th>
-        </tr>
-      </thead>
-      <div class="all-setlist">
-        <tbody>
-            @foreach ($setlists as $setlist)
-              <tr>
-                  <td>{{$setlist->id}}</td>
-                  <td>{{ date('Y.m.d', strtotime($setlist->date)) }}</td>
-                  @if(isset($setlist->artist_id))
-                  <td class="pc_list">
-                    <a href="{{ url('artists', $setlist->artist_id)}}">{{ $setlist->artist->name }}</a>
-                  </td>
-                  <td class="sp">
-                    <a href="{{ url('artists', $setlist->artist_id)}}">{{ $setlist->artist->name }}</a> /  <a href="{{ route('setlists.show', $setlist->id) }}">{{ $setlist->title }}</a>
-                  </td>
-                  @else
-                  <td class="pc_list"></td>
-                  <td class="sp"><a href="{{ route('setlists.show', $setlist->id) }}">{{ $setlist->title }}</a></td>
-                  @endif
-                  <td class="pc_list"><a href="{{ route('setlists.show', $setlist->id) }}">{{ $setlist->title }}</a></td>
-                  <td class="pc_list">{{ $setlist->venue }}</td>
-              </tr>
-            @endforeach
-        </tbody>
-      </div>
-    </table>
-  <div class=”pagination”>
-    {!! $setlists->links() !!}
-  </div>
-  <br>
-</div>
 @endsection
