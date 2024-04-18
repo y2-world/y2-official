e2eddbb73f85fa341726b79edb6e4100c51dbc3a
Yuki <yuki92496@gmail.com>
2024-04-18 23:25:05 +0900

    include added

--- resources/views/bios/index.blade.php
+++ resources/views/bios/index.blade.php
@@ -31,7 +31,7 @@
     </style> --}}
     <br>
     <div class="container">
-        <div class="parts-wrapper">
+        <div class="database-wrapper">
             <h2>Mr.Children Database</h2>
             {{-- <div class="search">
                 <form action="{{ url('/find') }}" method="GET">
@@ -40,12 +40,28 @@
                     </div>
                 </form>
             </div> --}}
-            <select name="select" onChange="location.href=value;">
-                <option value="" disabled selected>Select song</option>
-                @foreach ($songs as $song)
-                <option value="{{ url('songs/' . $song->id) }}">{{ $song->title }}</option>
-                @endforeach
-            </select>
+            <div>
+                <select name="select" onChange="location.href=value;">
+                    <option value="" disabled selected>Select song</option>
+                    @foreach ($songs as $song)
+                    <option value="{{ url('songs/' . $song->id) }}">{{ $song->title }}</option>
+                    @endforeach
+                </select>
+                <div class="row">
+                    <div class="col-md-3">
+                      <div class="form-check">
+                        <input class="form-check-input" type="radio" name="include" id="allRadio" value="1" checked>
+                        <label class="form-check-label" for="allRadio">All</label>
+                      </div>
+                    </div>
+                    <div class="col-md-9">
+                      <div class="form-check">
+                        <input class="form-check-input" type="radio" name="include" id="includeSoloRadio" value="2">
+                        <label class="form-check-label" for="includeSoloRadio">Not Include Solo</label>
+                    </div>
+                    </div>
+                  </div>
+            </div>
         </div>
         <hr>
         <h4 class="bio">Discography</h4>
