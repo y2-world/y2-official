a5cf80078d3377c7e8b4c1e974559d333ad29b06
Yuki <yuki92496@gmail.com>
2024-05-08 01:16:43 +0900

    hr changed

--- resources/views/live/show.blade.php
+++ resources/views/live/show.blade.php
@@ -63,7 +63,7 @@
                                                 <hr width="250">
                                             @elseif($data['#'] == 'hr')
                                                 </ol>
-                                                <hr width="80%">
+                                                <hr width="100%">
                                                 <ol>
                                             @else
                                                 <h5>{{ $data['#'] }} </h5>
