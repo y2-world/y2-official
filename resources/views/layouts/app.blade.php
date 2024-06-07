77be99e76307727e0db5bcba411e73d277d77f5a
Yuki <yuki92496@gmail.com>
2024-06-07 22:47:32 +0900

    home disco revised

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/e47a10189c.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{ asset('/css/main.css')  }}">
+    <link rel="stylesheet" href="{{ asset('/css/main.css?ver=20240607')  }}">
 </head>
 <div class="container">
     <div class="nav">
