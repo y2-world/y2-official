48a1ee68f955108e8a340250b2322604770b0e28
Yuki <yuki92496@gmail.com>
2024-06-07 21:47:56 +0900

    home disco revised

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -15,9 +15,7 @@
             <div class="row justify-content-center">
                 <div class="col-xl-11">
                     <!-- <div class="element js-fadein"> -->
-                    <div class="pc">
-                        <h3 class="news-title">New Release</h3>
-                    </div>
+                    <h3 class="news-title">New Release</h3>
                     <div class="cover-wrapper">
                         @foreach ($discos as $disco)
                             <div class="disc-block">
