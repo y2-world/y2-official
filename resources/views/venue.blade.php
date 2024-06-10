d9f37a5da650a8cf19e2b78cc7efdaf23f6490ec
Yuki <yuki92496@gmail.com>
2024-06-10 19:34:47 +0900

    venue search

--- resources/views/venue.blade.php
+++ resources/views/venue.blade.php
@@ -35,6 +35,7 @@
                         <th class="mobile">開催日</th>
                         <th class="pc">アーティスト</th>
                         <th class="sp">アーティスト / タイトル</th>
+                        <th class="mobile">タイトル</th>
                     </tr>
                 </thead>
                 <tbody>
