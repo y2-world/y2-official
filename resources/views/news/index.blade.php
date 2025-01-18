1e3d17523952a0087834285a61f052ac0bf38e7a
Yuki <yuki92496@gmail.com>
2025-01-18 16:56:14 +0900

    css revised

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -7,18 +7,18 @@
                 <div class="col-md-8">
                     <h2 class="news-title">News</h2>
                     <div class="element js-fadein">
-                        <hr>
                         <div class="element js-fadein">
                             @foreach ($news as $new)
-                                <a href="{{ route('news.show', $new->id) }}">
-                                    <div class="date">{{ date('Y.m.d', strtotime($new->date)) }}</div>
-                                    <h6>{{ $new->title }}</h6>
-                                    <?php
-                                    $text = strip_tags($new->text);
-                                    ?>
-                                    <p class="text">{{ Str::limit($text, 120, '…') }}</p>
-                                    <hr>
-                                </a>
+                                <div class="news-item">
+                                    <a href="{{ route('news.show', $new->id) }}" class="news-link">
+                                        <div class="date">{{ date('Y.m.d', strtotime($new->date)) }}</div>
+                                        <h6>{{ $new->title }}</h6>
+                                        <?php
+                                        $text = strip_tags($new->text);
+                                        ?>
+                                        <p class="text">{{ Str::limit($text, 120, '…') }}</p>
+                                    </a>
+                                </div>
                             @endforeach
                         </div>
                         <div class=”pagination”>
