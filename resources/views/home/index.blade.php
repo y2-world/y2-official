c72ccd7ab85eb28f6162d22e169e3e827b8c3317
Yuki <yuki92496@gmail.com>
2025-01-23 00:01:23 +0900

    css revised

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -48,7 +48,7 @@
         <div class="popup-content">
             <span class="close-btn">&times;</span>
             <div class="news-item">
-                <div id="popup-title" style="padding-right: 10px;"></h4>
+                <div id="popup-title" style="padding-right: 10px;"><div>
                 <small class="date" id="popup-date"></small>
                 <hr>
                 <div class="text" id="popup-text"></div>
