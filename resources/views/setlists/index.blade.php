e5e383e15a5dee0e4f01ac488802e855ea885008
Yuki <yuki92496@gmail.com>
2025-02-01 19:47:18 +0900

    artists added

--- resources/views/setlists/index.blade.php
+++ resources/views/setlists/index.blade.php
@@ -28,7 +28,7 @@
                         <div class="mb_dropdown">
                             <select name="artist_id" required data-toggle="select">
                                 <option value="" disabled selected>Artists</option>
-                                <option value="">No Artist Selected</option>
+                                <option value="">(No Artist Selected)</option>
                                 @foreach ($allArtists as $artist)
                                     <option value="{{ $artist->id }}" required>{{ $artist->name }}</option>
                                 @endforeach
