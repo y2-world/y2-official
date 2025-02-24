dce04bc1d6be9324670b70c29c909459b7399891
Yuki <yuki92496@gmail.com>
2025-02-25 00:37:06 +0900

    https fixed

--- public/vendor/laravel-admin/laravel-admin/laravel-admin.js
+++ public/vendor/laravel-admin/laravel-admin/laravel-admin.js
@@ -32,7 +32,9 @@ $(document).pjax('a:not(a[target="_blank"])', {
 
 // HTTP → HTTPS の問題を修正
 $(document).on('pjax:send', function (event, xhr, options) {
-    options.url = options.url.replace(/^http:/, 'https:');
+    if (options.url.startsWith('http://')) {
+        options.url = options.url.replace(/^http:/, 'https:');
+    }
 });
 
 NProgress.configure({ parent: '#app' });
