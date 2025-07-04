619fadb96f08681749bcfc934f355d2f70647276
Yuki <yuki92496@gmail.com>
2025-07-04 20:53:22 +0900

    Button fixed

--- public/js/admin-custom.js
+++ public/js/admin-custom.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const removeButtons = document.querySelectorAll('.has-many-setlist-form .remove, .has-many-encore-form .remove, .has-many-fes_setlist-form .remove, .has-many-fes_encore-form .remove');
-  removeButtons.forEach(function (btn) {
-    btn.childNodes.forEach(function (node) {
-      if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() === '消去') {
-        node.remove();
-      }
-    });
-  });
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//   const removeButtons = document.querySelectorAll('.has-many-setlist-form .remove, .has-many-encore-form .remove, .has-many-fes_setlist-form .remove, .has-many-fes_encore-form .remove');
+//   removeButtons.forEach(function (btn) {
+//     btn.childNodes.forEach(function (node) {
+//       if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() === '消去') {
+//         node.remove();
+//       }
+//     });
+//   });
+// });
