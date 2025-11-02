58ed6fb510d60e3e6a95fec368bb44287cf8d05d
Yuki <yuki92496@gmail.com>
2025-11-02 21:58:58 +0900

    エラー修正

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,7 +5,7 @@
     ?>
     <div class="cover">
         <div class="element js-fadein">
-            <img src={{ asset('images/top_image.jpg') }}>
+            <img src="{{ asset('images/top_image.jpg') }}">
             <p><span>Yuki Yoshida</span> <span>Official Website</span></p>
         </div>
     </div>
@@ -17,7 +17,6 @@
                     <div class="element js-fadein">
                         <h2 class="news-title">News</h2>
 
-                        <!-- ニュースリストを格納するコンテナ -->
                         <div id="news-container">
                             @foreach ($news as $new)
                                 @if ($new->visible == 1)
@@ -33,7 +32,6 @@
                             @endforeach
                         </div>
 
-                        <!-- View Allボタン -->
                         <div class="more">
                             <a href="javascript:void(0);" id="view-all-news-btn">View All</a>
                         </div>
@@ -48,11 +46,11 @@
         <div class="popup-content">
             <span class="close-btn">&times;</span>
             <div class="news-item">
-                <div id="popup-title" style="padding-right: 10px; display: inline-block;"></div> <!-- タイトル -->
-                <small class="date" id="popup-date"></small> <!-- 日付 -->
+                <div id="popup-title" style="padding-right: 10px; display: inline-block;"></div>
+                <small class="date" id="popup-date"></small>
                 <hr>
-                <div class="text" id="popup-text"></div> <!-- テキスト -->
-                <img class="image" width="100%"  style="padding-top: 10px;" id="popup-img"> <!-- 画像 -->
+                <div class="text" id="popup-text"></div>
+                <img class="image" width="100%" style="padding-top: 10px;" id="popup-img">
             </div>
         </div>
     </div>
@@ -63,23 +61,13 @@
                 <div class="col-xl-9">
                     <div class="element js-fadein">
                         <h2 class="music-header">Music</h2>
-                        {{-- <ul class="music-menu">
-                            <li><a href="{{ url('/music') }}" class="{{ request('type') == '' ? 'active' : '' }}">All</a>
-                            </li>
-                            <li><a href="{{ url('/music?type=1') }}"
-                                    class="{{ request('type') == '1' ? 'active' : '' }}">Single</a>
-                            </li>
-                            <li><a href="{{ url('/music?type=2') }}"
-                                    class="{{ request('type') == '2' ? 'active' : '' }}">Album</a>
-                            </li>
-                        </ul> --}}
                         <div class="music-wrapper">
                             <div class="album-wrapper album-wrapper-scroll" id="music-container">
                                 <div class="album-wrapper-inner">
                                     @foreach ($discos as $disc)
                                         <div class="album-container">
                                             <a href="{{ route('music.show', $disc->id) }}">
-                                                <img src="{{ asset('https://res.cloudinary.com/hqrgbxuiv/' . $disc->image) }}"
+                                                <img src="https://res.cloudinary.com/hqrgbxuiv/{{ $disc->image }}"
                                                     class="album-image">
                                             </a>
                                             <div class="music-item__gray">
@@ -93,7 +81,7 @@
                                     @foreach ($discos as $disc)
                                         <div class="album-container">
                                             <a href="{{ route('music.show', $disc->id) }}">
-                                                <img src="{{ asset('https://res.cloudinary.com/hqrgbxuiv/' . $disc->image) }}"
+                                                <img src="https://res.cloudinary.com/hqrgbxuiv/{{ $disc->image }}"
                                                     class="album-image">
                                             </a>
                                             <div class="music-item__gray">
@@ -108,7 +96,6 @@
                             </div>
                         </div>
 
-                        <!-- View Allボタン -->
                         <div class="more">
                             <a href="javascript:void(0);" id="view-all-music-btn">View All</a>
                         </div>
@@ -129,7 +116,7 @@
                                 @foreach ($profiles as $profile)
                                     <div class="col-lg-6 my-auto">
                                         <div class="prof_img">
-                                            <img src={{ asset('https://res.cloudinary.com/hqrgbxuiv/' . $profile->image) }}
+                                            <img src="https://res.cloudinary.com/hqrgbxuiv/{{ $profile->image }}"
                                                 class="image" width="80%">
                                         </div>
                                     </div>
@@ -183,7 +170,7 @@
                         <a href="{{ url('/#music') }}">Music</a>・
                         <a href="{{ url('/#profile') }}">Profile</a>・
                         <a href="{{ url('/#radio') }}">Radio</a>・
-                        <a href="{{ url('https://ameblo.jp/y2-world') }}" target="_blank">Blog</a>・
+                        <a href="https://ameblo.jp/y2-world" target="_blank">Blog</a>・
                         <a href="{{ url('/admin') }}" target="_blank">Admin</a>
                         <br>
                         <div class="footer-copyright">©2024 y2 records inc.</div>
@@ -196,6 +183,6 @@
 @endsection
 
 @section('page-script')
-<script src="{{ asset('/js/search.js?time=' . time()) }}"></script>
-<script src="{{ asset('/js/top.js?time=' . time()) }}"></script>
+    <script src="{{ asset('/js/search.js?time=' . time()) }}"></script>
+    <script src="{{ asset('/js/top.js?time=' . time()) }}"></script>
 @endsection
